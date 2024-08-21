55f283ac49b0134e04057e6247dfd900941464a1
douglasferreirag <douglas.ferreirag@gmail.com>
2024-08-20 22:03:01 -0300

    Tabuada virtual

---
+++ src/App.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     
+            
+        System.out.println("Tabuada virtual");
+        
+        for(int i = 1; i <= 10; i++){
+
+            System.out.println("Tabuada do " + i);
+
+            System.out.println();
+
+            System.out.println();
+
+            for(int j = 1; j <= 10; j++){
+
+                System.out.println(i + " x " + j +  " = " + i * j);
+
+                
+
+
+
+            }
+
+            System.out.println();
+
+            System.out.println();
+        }
+
+    }
+}
